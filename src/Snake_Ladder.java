c2dc3c285ecd6d629ce5c99350bc0c392134b0c3
Manish241099 <manishkhade241099@gmail.com>
2024-05-21 23:50:16 -0700

    UC4_USING_WHILE

--- src/Snake_Ladder.java
+++ src/Snake_Ladder.java
@@ -14,19 +14,28 @@ public class Snake_Ladder {
         System.out.println("welcome to snake and ladder");
         int Win_condition = 100;
         int player_position = 1;
+        while (player_position < 100) {
         int dice_roll = Diceroll();
         int option = Option();
-        if(option ==1){
-            player_position +=dice_roll;
-        }
-        if(option ==2 ){
-            player_position -= dice_roll;
-        }
+        int prev_pos=0;
+            if(player_position+dice_roll >100){
+                player_position =prev_pos;
+            }
+            if (option == 1) {
+                player_position += dice_roll;
+            }
+            if (option == 2) {
+                player_position -= dice_roll;
+            }
+
         if(option == 3);
         System.out.println("Dice roll is "+ dice_roll);
         System.out.println("Player Position is "+player_position);
 
 
+
+        }
     }
+
 }
 
