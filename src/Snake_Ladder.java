f24d8f378a93e155696e56227664581a076cd411
Manish241099 <manishkhade241099@gmail.com>
2024-05-22 00:05:45 -0700

    UC5_added

--- src/Snake_Ladder.java
+++ src/Snake_Ladder.java
@@ -1,24 +1,26 @@
+import java.lang.Math;
 public class Snake_Ladder {
+    int WIN_CONDITION = 100;
+
     public static int Diceroll() {
-        int diceroll = (int) (Math.random() * 6) + 1;
-        return diceroll;
+        return (int) (Math.random() * 6) + 1;
 
     }
 
     public static int Option() {
-        int option = (int) (Math.random() * 3) + 1;
-        return option;
+        return (int) (Math.random() * 3) + 1;
+
     }
 
     public static void main(String[] args) {
-        System.out.println("welcome to snake and ladder");
-        int Win_condition = 100;
+        int WIN_CONDITION = 100;
+
         int player_position = 1;
-        while (player_position < 100) {
-        int dice_roll = Diceroll();
-        int option = Option();
-        int prev_pos=0;
-            if(player_position+dice_roll >100){
+        int prev_pos=1;
+        while (player_position < WIN_CONDITION) {
+            int dice_roll = Diceroll();
+            int option = Option();
+            if(player_position+dice_roll >WIN_CONDITION){
                 player_position =prev_pos;
             }
             if (option == 1) {
@@ -27,15 +29,18 @@ public class Snake_Ladder {
             if (option == 2) {
                 player_position -= dice_roll;
             }
+            if(player_position<0){
+                player_position =0;
+            }
 
-        if(option == 3);
-        System.out.println("Dice roll is "+ dice_roll);
-        System.out.println("Player Position is "+player_position);
-
-
+            System.out.println("Dice roll is " + dice_roll);
+            System.out.println(option);
+            System.out.println("Player Position is " + player_position);
+            if(player_position==WIN_CONDITION){
+                System.out.println("Player Win");
+                break;
+            }
 
         }
     }
-
 }
-
