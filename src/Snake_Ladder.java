87416be2d6f8d4f9efedf00856cebfed874047d5
Manish241099 <manishkhade241099@gmail.com>
2024-05-21 22:37:53 -0700

    UC1_Pos_Init

---
+++ src/Snake_Ladder.java
@@ -0,0 +1,9 @@
+public class Snake_Ladder {
+
+    public static void main(String[] args) {
+        System.out.println("welcome to snake and ladder");
+        int Win_condition =100;
+        int player_position =1;
+
+    }
+}
