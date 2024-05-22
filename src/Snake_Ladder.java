c8239eff66953b4ea0013b009068d6689e81f704
Manish241099 <manishkhade241099@gmail.com>
2024-05-21 23:33:18 -0700

    UC3

--- src/Snake_Ladder.java
+++ src/Snake_Ladder.java
@@ -1,13 +1,32 @@
 public class Snake_Ladder {
-    public static int Diceroll(){
-        int diceroll = (int)(Math.random()*6) +1;
+    public static int Diceroll() {
+        int diceroll = (int) (Math.random() * 6) + 1;
         return diceroll;
 
+    }
 
-        public static void main(String[] args) {
+    public static int Option() {
+        int option = (int) (Math.random() * 3) + 1;
+        return option;
+    }
+
+    public static void main(String[] args) {
         System.out.println("welcome to snake and ladder");
-        int Win_condition =100;
-        int player_position =1;
+        int Win_condition = 100;
+        int player_position = 1;
+        int dice_roll = Diceroll();
+        int option = Option();
+        if(option ==1){
+            player_position +=dice_roll;
+        }
+        if(option ==2 ){
+            player_position -= dice_roll;
+        }
+        if(option == 3);
+        System.out.println("Dice roll is "+ dice_roll);
+        System.out.println("Player Position is "+player_position);
+
 
     }
 }
+
