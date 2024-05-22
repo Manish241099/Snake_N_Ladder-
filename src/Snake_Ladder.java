a7abb3e9df411834138a748beffce3c794aabdaa
Manish241099 <manishkhade241099@gmail.com>
2024-05-21 22:56:45 -0700

    UC2_DICE_FUNCTION

--- src/Snake_Ladder.java
+++ src/Snake_Ladder.java
@@ -1,6 +1,10 @@
 public class Snake_Ladder {
+    public static int Diceroll(){
+        int diceroll = (int)(Math.random()*6) +1;
+        return diceroll;
 
-    public static void main(String[] args) {
+
+        public static void main(String[] args) {
         System.out.println("welcome to snake and ladder");
         int Win_condition =100;
         int player_position =1;
